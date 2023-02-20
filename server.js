5be175739c7d069b1a7dccd6668cb172fd86bc53
noman598 <106810794+noman598@users.noreply.github.com>
2023-02-20 13:14:13 +0530

    Explaination

--- server.js
+++ server.js
@@ -5,7 +5,21 @@ const bodyparser = require("body-parser");
 // it is faster than alone NodeJS.
 
 var app = express()
+
+// What is used of bodyparser ? => Using bodyparser we can grab the data of the index file (i.e. input data by using the same name). This is common code for everycase.
+
 app.use(bodyparser.urlencoded({extende: true}));
+
+
+// instaed of sending html section code, can be send the whole file to the browser from the localhost.
+
+// And to Avoid error on file path we are using "__dirname". it directly get the path No matter where is you file.
+
+// Inside of index.html file -> Used a form with action and method -
+
+// <form action = "/" method = Post> 
+
+// Besically here the Post -> post method is used to send the form data to the server on (action = "/") home position.
 
 
 app.get('/', function(req, res){
@@ -15,8 +29,12 @@ app.get('/bmicalculator', function(req, res){
     res.sendFile(__dirname + "/bmiCalculator.html");
 })
 
+// By Post function, if we are posting something from the index file page. So it go to the new page but with the given url and give the desire result.
 
 app.post('/bmicalculator', function(req, res){
+    
+//  By using req.body.height = > it grab the data on the particular name height from the "/bmicalculator" page.Number is for making integer if the fetching data. 
+    
     var height = Number(req.body.height);
     var weight = Number(req.body.weight);
 
